77792cdbd3577be8db558e67450fc7c1a27adeaf
edu8686 <85796665+edu8686@users.noreply.github.com>
2023-04-27 15:07:36 -0300

    reacomodamiento de archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,47 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcercaComponent } from './Componentes/acerca/acerca.component';
-import { NavbarComponent } from './Componentes/navbar/navbar.component';
 import { EncabezadoComponent } from './Componentes/encabezado/encabezado.component';
 import { EducacionComponent } from './Componentes/educacion/educacion.component';
 import { ProyectosComponent } from './Componentes/proyectos/proyectos.component';
 import { LogInComponent } from './Componentes/login/login.component';
 import { PortfolioComponent } from './Componentes/portfolio/portfolio.component';
+import { BannerComponent } from './Componentes/banner/banner.component';
+import { ExperienciaLaboralComponent } from './Componentes/experiencia-laboral/experiencia-laboral.component';
+import { HardSoftSkillsComponent } from './Componentes/hard-soft-skills/hard-soft-skills.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { FooterComponent } from './Componentes/footer/footer.component';
+import { NavbarComponent } from './Componentes/navbar/navbar.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     AcercaComponent,
-    NavbarComponent,
     EncabezadoComponent,
     EducacionComponent,
     ProyectosComponent,
     LogInComponent,
     PortfolioComponent,
+    BannerComponent,
+    ExperienciaLaboralComponent,
+    HardSoftSkillsComponent,
+    FooterComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [],
   bootstrap: [AppComponent]
